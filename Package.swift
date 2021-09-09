31fbe652c3b5547ab4f7e289911ec39cec6336bc
Callum McColl <callum.mccoll1@gmail.com>
2021-09-09 16:15:13 +1000

    Always use github for swift_helpers

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["whiteboard_helpers"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mipalgu/swift_helpers.git", .branch("master"))
+        .package(url: "https://github.com/mipalgu/swift_helpers.git", .branch("main"))
     ],
     targets: [
         .target(
